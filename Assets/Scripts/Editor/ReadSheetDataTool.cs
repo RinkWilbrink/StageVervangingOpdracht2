80906f6dc137aaba86094b83c78231e66269b4d4
Rink Wilbrink <rinkwilbrink1@gmail.com>
2020-09-28 11:57:13 +0200

    changed soem stuff

--- Assets/Scripts/Editor/ReadSheetDataTool.cs
+++ Assets/Scripts/Editor/ReadSheetDataTool.cs
@@ -17,6 +17,8 @@ public class ReadSheetDataTool : EditorWindow
     // Variable
     private string CSVFileName;
 
+    private string ScriptPath;
+
     //private string[,] LinesAndRows;
 
     [MenuItem("ReadSheetDataTool/Tool")]
@@ -33,7 +35,7 @@ public class ReadSheetDataTool : EditorWindow
 
         GUILayout.Space(6);
 
-        if (GUILayout.Button("Banaan"))
+        if (GUILayout.Button("Reading Data From Data Sheet"))
         {
             string[] linesArray = System.IO.File.ReadAllText(Application.dataPath.Replace("Assets", CSVFileName)).Split('\n');
 
@@ -61,7 +63,20 @@ public class ReadSheetDataTool : EditorWindow
                 for (int y = 0; y < LinesAndRows.GetLength(1); y++)
                 {
                     //Debug.Log()
-                    LinesAndRows[x, y] = Y[y];
+                    try
+                    {
+                        LinesAndRows[x, y] = Y[y];
+                    }
+                    catch
+                    {
+                        //LinesAndRows[x, y] = "";
+                        Debug.LogErrorFormat("Error with adding the Y[{1}] to LinesAndRows[{0}, {1}]\n Set {0}, {1} to whitetext! check the Sheet file for empty Cells", x, y);
+
+                        // Reset values to prevent parts of the array to be useable
+                        x = LinesAndRows.GetLength(0);
+                        y = LinesAndRows.GetLength(1);
+                        LinesAndRows = null;
+                    }
 
                     //Debug.Log(LinesAndRows[x, y]);
                 }
@@ -69,18 +84,13 @@ public class ReadSheetDataTool : EditorWindow
 
             ReadData();
         }
+    }
 
-        if (GUILayout.Button("adaw"))
-        {
-            string[] SPLIT = ",\n".Split(',');
+    private void SaveFileName()
+    {
+        ScriptPath = AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this));
 
-            Debug.Log(SPLIT.Length);
 
-            for (int i = 0; i < SPLIT.Length; i++)
-            {
-                Debug.Log(SPLIT[i]);
-            }
-        }
     }
 
     private void ReadData()
